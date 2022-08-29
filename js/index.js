0d966b807497788bc1afec4d92231a2489974511
djftrainer <djftrainer@gmail.com>
2022-08-29 20:41:26 -0300

    nuevos cambios

---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal ();
+   sr.reveal ('.cajaleft ',{
+    duration: 4000,
+    origin: 'left' ,
+    distance: '400px'
+   });
+   sr.reveal ('.cajaright ',{
+    duration: 4000,
+    origin: 'right' ,
+    distance: '400px'
+   }
+   );
+   sr.reveal ('.pindex ',{
+    duration: 3000,
+    origin: 'bottom' ,
+    distance: '-100px'});
+
+   sr.reveal ('.general ',{
+      duration: 3000,
+      origin: 'right',
+      distance: '400px' 
+    })
